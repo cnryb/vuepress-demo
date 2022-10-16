7165d0f30769d4850083a459224b940c17eb7927
任跃兵 <cnryb@live.cn>
2022-10-16 18:05:13 +0800

    config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,7 @@
-module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around',
-    base:'vuepress-demo'
-  }
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  base: 'vuepress-demo'
+})
